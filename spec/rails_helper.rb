9073ca2244145517d3689f10a2f9736b19101c7c
pauline rouger <paulinerouge@hotmail.com>
2022-11-21 22:31:01 +0800

    Setup automated login in test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Setup automated login in test
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include Warden::Test::Helpers
+
 end
