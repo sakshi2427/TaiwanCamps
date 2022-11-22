148e485225677e05a857bd81566b30fe0e01ccac
pauline rouger <paulinerouge@hotmail.com>
2022-11-22 22:10:57 +0800

    Refactor User rspec test using gem shoulda-matchers

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   validates_presence_of :email, :first_name, :last_name
   validates_uniqueness_of :email
   validates_length_of :first_name, :last_name, maximum: 20
-  validates_format_of :email,:with => Devise::email_regexp
+  validates_length_of :password, minimum: 6
+  validates_format_of :email, with: Devise::email_regexp
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,70 +2,72 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  # subject {
-  # described_class.new(first_name: "Pauline",
-  #                     last_name: "Rouger",
-  #                     email: "test@test.com",
-  #                     password: "123456",)
-  # }
+  describe 'User validations' do
+    # Here using FactoryBot
+    before { FactoryBot.build(:user) }
 
-  subject { FactoryBot.build(:user) }
-
-  describe "Validations" do
-    it "is valid with valid attributes" do
-      expect(subject).to be_valid
-    end
-
-    it 'is not valid without a first name' do
-      subject.first_name = nil
-      expect(subject).not_to be_valid
-    end
-
-    it "is not valid with a empty first name" do
-      subject.first_name = ""
-      expect(subject).not_to be_valid
-    end
-
-      it 'is not valid without a last name' do
-      subject.last_name = nil
-      expect(subject).not_to be_valid
-    end
-
-    it "is not valid with a empty last name" do
-      subject.last_name = ""
-      expect(subject).not_to be_valid
-    end
-
-    it "is not valid without an email" do
-      subject.email = nil
-      expect(subject).to_not be_valid
-    end
-
-    it "is not valid with a empty email" do
-      subject.email = ""
-      expect(subject).to_not be_valid
-    end
-
-    it "is not valid without a password" do
-      subject.password = nil
-      expect(subject).to_not be_valid
-    end
-
-    it "is not valid with a empty password" do
-      subject.password = ""
-      expect(subject).not_to be_valid
-    end
-
-    # it 'should validate uniqueness of email' do
-    #   email1 = FactoryBot.create(:user, email: 'email@blackducksoftware.com')
-    #   email2 = FactoryBot.build(:user, email: 'email@blackducksoftware.com' )
-    #   should validate_uniqueness_of(:email)
-    # end
-
-    describe "Uniqueness" do
-      subject { FactoryBot.build(:user) }
-      it { should validate_uniqueness_of(:email) }
-    end
-
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_length_of(:first_name).is_at_most(20) }
+    it { should validate_length_of(:last_name).is_at_most(20) }
+    it { should validate_length_of(:password).is_at_least(6) }
+    it { should allow_value("email@address.foo").for(:email) }
+    it { should_not allow_value("foo").for(:email) }
   end
+
+  # subject { FactoryBot.build(:user) }
+
+  # describe "validations" do
+
+  #   it "is valid with valid attributes" do
+  #     expect(subject).to be_valid
+  #   end
+
+  #   # testing shoulda-matchers
+  #   it { should validate_presence_of(:first_name) }
+  #   it { is_expected.to validate_presence_of(:name) }
+
+  #   it 'is not valid without a first name' do
+  #     subject.first_name = nil
+  #     expect(subject).not_to be_valid
+  #   end
+
+  #   it "is not valid with a empty first name" do
+  #     subject.first_name = ""
+  #     expect(subject).not_to be_valid
+  #   end
+
+  #     it 'is not valid without a last name' do
+  #     subject.last_name = nil
+  #     expect(subject).not_to be_valid
+  #   end
+
+  #   it "is not valid with a empty last name" do
+  #     subject.last_name = ""
+  #     expect(subject).not_to be_valid
+  #   end
+
+  #   it "is not valid without an email" do
+  #     subject.email = nil
+  #     expect(subject).to_not be_valid
+  #   end
+
+  #   it "is not valid with a empty email" do
+  #     subject.email = ""
+  #     expect(subject).to_not be_valid
+  #   end
+
+  #   it "is not valid without a password" do
+  #     subject.password = nil
+  #     expect(subject).to_not be_valid
+  #   end
+
+  #   it "is not valid with a empty password" do
+  #     subject.password = ""
+  #     expect(subject).not_to be_valid
+  #   end
+  # end
 end
