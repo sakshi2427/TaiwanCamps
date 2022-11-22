ae440a13fa6d99417d0657d816f15460af5bcc98
pauline rouger <paulinerouge@hotmail.com>
2022-11-22 15:45:20 +0800

    Added FactoryBot

---
+++ spec/factories/factories.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:email) { |n| "test-#{n.to_s.rjust(3, "0")}@sample.com" }
+    password { "123456" }
+    first_name { "John" }
+    last_name { "Doe" }
+  end
+end
