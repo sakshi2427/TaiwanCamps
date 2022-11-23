d924f6de2433f8d2f8d07b6b03867c852227410b
pauline rouger <paulinerouge@hotmail.com>
2022-11-23 21:46:15 +0800

    Writing first integration test (creating a campsite)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,22 +7,25 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
+    # @review.user_id = @user.id
+    @review.user = @user
+    # @review.campsite_id = @campsite.id
     @review.campsite = @campsite
-    @review.save
-    if @review.save
-      redirect_to campsite_path(@campsite)
-    else
-      render :new, status: :unprocessable_entity
-    end
-    # respond_to do |format|
-    #   if @review.save
-    #     format.html { redirect_to restaurant_path(@restaurant, anchor: "review-#{@review.id}") }
-    #     format.json # Follow the classic Rails flow and look for a create.json view
-    #   else
-    #     format.html { render 'restaurants/show' }
-    #     format.json # Follow the classic Rails flow and look for a create.json view
-    #   end
+    # @review.save
+    # if @review.save
+    #   redirect_to campsite_path(@campsite)
+    # else
+    #   render :new, status: :unprocessable_entity
     # end
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to campsite_path(@campsite, anchor: "review-#{@review.id}") }
+        format.json # Follow the classic Rails flow and look for a create.json view
+      else
+        format.html { render 'campsites/show' }
+        format.json # Follow the classic Rails flow and look for a create.json view
+      end
+    end
   end
 
   private
