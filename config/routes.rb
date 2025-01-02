4a84ed9def5d54210dcd3d451c5b04e77638c01c
sakshi2427 <sakshikulshrestha234@gmail.com>
2025-01-02 12:12:39 +0530

    issue 1 fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
-Rails.application.routes.draw do
-  get 'campsites/index'
-  get 'campsites/show'
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  scope '(:locale)', locale: /en|zh/ do
-    root to: 'pages#home'
-    resources :campsites do
-      resources :reviews, only: [:new, :create]
-    end
-  end
+Rails.application.routes.draw do 
+  get 'campsites/index' 
+  get 'campsites/show' 
+  devise_for :users 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html 
+  # Defines the root path route ("/") 
+  # root "articles#index" 
+  scope '(:locale)', locale: /en|zh/ do 
+    root to: 'pages#home' 
+    resources :campsites do 
+      resources :reviews, only: [:new, :create] 
+      get 'new', to: 'campsites#new', as: 'new_campsite' # INPUT_REQUIRED {This route allows users to access the campsite creation form} 
+      post 'create', to: 'campsites#create', as: 'create_campsite' # Ensure to handle the form submission 
+    end 
+  end 
 end
--- spec/features/create_campsite_spec.rb
+++ spec/features/create_campsite_spec.rb
@@ -1,18 +1,28 @@
-require 'rails_helper'
-
-RSpec.describe 'Creating a campsite', type: :feature do
-  scenario 'valid inputs' do
-    visit new_campsite_path
-    fill_in 'Name', with: 'Test Campsite'
-    click_on 'Create campsite'
-    visit campsites_path
-    expect(page).to have_content('Test Campsite')
-  end
-
-  scenario 'invalid inputs' do
-    visit new_campsite_path
-    fill_in 'Name', with: ''
-    click_on 'Create campsite'
-    expect(page).to have_content("Name can't be blank")
-  end
+require 'rails_helper' 
+RSpec.describe 'Creating a campsite', type: :feature do 
+  context 'when user is logged in' do 
+    before do 
+      @user = FactoryBot.create(:user) 
+      login_as(@user, scope: :user) # Assuming you have a login helper 
+    end 
+    scenario 'valid inputs' do 
+      visit new_campsite_path 
+      fill_in 'Name', with: 'Test Campsite' 
+      click_on 'Create campsite' 
+      visit campsites_path 
+      expect(page).to have_content('Test Campsite') 
+    end 
+    scenario 'invalid inputs' do 
+      visit new_campsite_path 
+      fill_in 'Name', with: '' 
+      click_on 'Create campsite' 
+      expect(page).to have_content("Name can't be blank") 
+    end 
+  end 
+  context 'when user is not logged in' do 
+    scenario 'redirects to login page' do 
+      visit new_campsite_path 
+      expect(page).to have_current_path(new_user_session_path) # Assuming Devise routes 
+    end 
+  end 
 end
